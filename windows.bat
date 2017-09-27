5e016deffe973fd45a442f73a2d54a8c4b6447ec
Tom Jones <tom.jones@blacksandseucry.com>
2017-09-27 13:32:33 -0400

    working on windows install bat file

--- windows.bat
+++ windows.bat
@@ -2,10 +2,10 @@ REM @echo off
 @REM @Author: Tom Jones
 @REM @Date:   2017-09-27 13:18:57
 @REM @Last Modified by:   Tom Jones
-@REM Modified time: 2017-09-27 13:21:44
+@REM Modified time: 2017-09-27 13:31:47
 
 echo RUN THIS SCRIPT AS AN ADMINISTRATOR
 
-certutil -addstore "Root" blacksands_ca_l3.crt
-certutil -addstore "CA" blacksands_ca_l3.crt
+certutil -addstore "Root" blacksands_inc_ca_root.crt
+certutil -addstore "CA" blacksands_inc_ca.crt
 certutil -addstore "CA" blacksands_ca_l3.crt
